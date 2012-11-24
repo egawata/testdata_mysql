b382cf6bcc4826ea2995f55b4d26a64a17815109
Egawa Takashi <egawa.takashi@gmail.com>
2012-11-24 18:13:05 +0900

    add foreign key func

--- misc/get_tabledef.pl
+++ misc/get_tabledef.pl
@@ -5,11 +5,20 @@ use warnings;
 use DBI;
 use Data::Dumper;
 use DateTime;
+use Time::HiRes qw(gettimeofday tv_interval);
 
 
+my $ONE_YEAR_SEC = 86400 * 365;
 
-my $dsn = 'dbi:mysql:dbname=test';
-my $user = 'root';
+my $DBNAME = 'test';
+
+my $DSN = "dbi:mysql:dbname=$DBNAME";
+my $USER = 'root';
+
+my $TABLE_NAME = 'test';
+my $REF_TABLE_NAME = 'ref_test';
+
+my %cond = ();
 
 my @VARCHAR_LIST = ( 0..9, 'a'..'z', 'A'..'Z', '_' );
 my $COUNT_VARCHAR_LIST = scalar @VARCHAR_LIST;
@@ -22,11 +31,14 @@ my $MAX_INT_SIGNED           = 2147483647;
 my $MAX_INT_UNSIGNED         = 4294967295;
 
 my %VALUE_DEF_FUNC = (
+    char        => \&val_varchar,
     varchar     => \&val_varchar,
     tinyint     => \&val_tinyint,
     smallint    => \&val_smallint,
     int         => \&val_int,
     datetime    => \&val_datetime,
+    timestamp   => \&val_datetime,
+    date        => \&val_datetime,
 );
 
 
@@ -36,28 +48,112 @@ exit(0);
 
 sub main {
 
-    my $dbh = DBI->connect($dsn, $user, undef, { AutoCommit => 1, RaiseError => 1 })
+    my $dbh = DBI->connect($DSN, $USER, undef, { AutoCommit => 1, RaiseError => 1 })
         or die $!;
 
-    my $def = get_table_definition($dbh);
+    $dbh->do("DROP TABLE IF EXISTS $TABLE_NAME");
+
+    $dbh->do("DROP TABLE IF EXISTS $REF_TABLE_NAME");
+
+    $dbh->do(<<SQL);
+        CREATE TABLE $REF_TABLE_NAME (
+            id integer primary key auto_increment,
+            name varchar(20) not null
+        )
+SQL
+
+    $dbh->do(<<SQL);
+        CREATE TABLE $TABLE_NAME (
+            id integer primary key auto_increment,
+            name varchar(20) not null,
+            price int unsigned not null,
+            amount int unsigned not null default 10,
+            madein char(2),
+            ref_table_id integer not null,
+            created datetime,
+            start_date date,
+            constraint foreign key (ref_table_id) references $REF_TABLE_NAME (id)
+        )
+SQL
+
+#    $dbh->do(<<SQL);
+#        INSERT INTO $REF_TABLE_NAME (name) values ('testname1'), ('testname2')
+#SQL
+
+
+    process_table($dbh, $TABLE_NAME) for 1..100;
+
+    $dbh->disconnect;
+
+}
+
+
+sub process_table {
+    my ($dbh, $table) = @_;
+
+    my $def = get_table_definition($dbh, $table);
     print Dumper($def);
 
-    my @colnames = grep { $def->{$_}{Extra} !~ /auto_increment/ } keys %$def;
+    my $constraint = get_constraint($dbh, $table);
+    print Dumper($constraint);
+
+    my @colnames = grep { 
+        $def->{$_}{EXTRA} !~ /auto_increment/
+        and not defined($def->{$_}{COLUMN_DEFAULT}) 
+    } keys %$def;
     my $cols = join ',', @colnames;
     my $ph   = join ',', ('?') x scalar(@colnames);
-    my $sql = "INSERT INTO test ($cols) VALUES ($ph)";
+    my $sql = "INSERT INTO $table ($cols) VALUES ($ph)";
 
     my $sth = $dbh->prepare($sql);
 
-    for ( 1..100 ) {
+    {
         my @values = ();
         for my $key (@colnames) {
             #print $def->{$key}{Type} . "\n";
-            my ($type, $size, $opt) = ($def->{$key}{Type} =~ /^(\w+)(?:\((\d+)\))?(\s\w+)?$/);
-            my $func = $VALUE_DEF_FUNC{$type}
-                or die "Type $type not supported";
-            
-            my $value = $func->($size, $opt);
+            my $type = $def->{$key}{DATA_TYPE};
+            my $size = $def->{$key}{CHARACTER_MAXIMUM_LENGTH};
+            my $opt  = $def->{$key}{COLUMN_TYPE};
+
+            #  外部キー制約の有無を確認
+            my $const_key = $constraint->{$key};
+            if ( $const_key->{REFERENCED_TABLE_SCHEMA} 
+                and my $ref_table = $const_key->{REFERENCED_TABLE_NAME} 
+                and my $ref_col   = $const_key->{REFERENCED_COLUMN_NAME} ) 
+            {
+                if ( !defined( $cond{$table}{$key} ) ) {
+                    my $ref_res = get_current_ref_keys($dbh, $ref_table, $ref_col); 
+                    if ( @$ref_res ) {
+                        $cond{$table}{$key}{random} = [ map { $_->[0] } @$ref_res ];
+                    }
+                    else {
+                        my $ref_keys = process_table($dbh, $ref_table);
+                        $ref_res = get_current_ref_keys($dbh, $ref_table, $ref_col);
+                        if ( @$ref_res ) {
+                            $cond{$table}{$key}{random} = [ map { $_->[0] } @$ref_res ];
+                        }
+                        else {
+                            die "Something wrong\n";
+                        }
+                    } 
+                }
+            }
+
+            my $value;
+            if ( my $cond_key = $cond{$table}{$key} ) {
+
+                if ( $cond_key->{random} ) {
+                    my $ind = rand() * @{ $cond_key->{random} };
+                    $value = $cond_key->{random}[$ind]; 
+                }
+
+            } 
+            else {
+                my $func = $VALUE_DEF_FUNC{$type}
+                    or die "Type $type for $key not supported";
+                
+                $value = $func->($size, $opt);
+            }
 
             push @values, $value;
         }
@@ -66,21 +162,28 @@ sub main {
     }
 
     $sth->finish;
-
-    $dbh->disconnect;
-
 }
 
 
-sub get_table_definition {
-    my ($dbh) = @_;
 
-    my $sql = "show full columns from test";
-    my $res = $dbh->selectall_hashref($sql, 'Field');
+sub get_table_definition {
+    my ($dbh, $table) = @_;
+
+    my $sql = "SELECT * FROM information_schema.columns WHERE table_schema = ? AND table_name = ?";
+    my $res = $dbh->selectall_hashref($sql, 'COLUMN_NAME', undef, $DBNAME, $table);
 
     return $res;
 }
 
+
+sub get_constraint {
+    my ($dbh, $table) = @_;
+
+    my $sql = "SELECT * FROM information_schema.key_column_usage where table_schema = ? AND table_name = ?";
+    my $res = $dbh->selectall_hashref($sql, 'COLUMN_NAME', undef, $DBNAME, $table);
+    
+    return $res;  
+}
 
 sub val_varchar {
     my ($size) = @_;
@@ -97,24 +200,33 @@ sub val_varchar {
 sub val_tinyint {
     my ($size, $opt) = @_;
 
-    return (($opt || '') eq 'unsigned') ? int(rand() * $MAX_TINYINT_UNSIGNED) : int(rand() * $MAX_TINYINT_SIGNED);
+    return (($opt || '') =~ /unsigned/) ? int(rand() * $MAX_TINYINT_UNSIGNED) : int(rand() * $MAX_TINYINT_SIGNED);
 }
 
 sub val_smallint {
     my ($size, $opt) = @_;
 
-    return (($opt || '') eq 'unsigned') ? int(rand() * $MAX_SMALLINT_UNSIGNED) : int(rand() * $MAX_SMALLINT_SIGNED);
+    return (($opt || '') =~ /unsigned/) ? int(rand() * $MAX_SMALLINT_UNSIGNED) : int(rand() * $MAX_SMALLINT_SIGNED);
 }
 
 sub val_int {
     my ($size, $opt) = @_;
 
-    return (($opt || '') eq 'unsigned') ? int(rand() * $MAX_INT_UNSIGNED) : int(rand() * $MAX_INT_SIGNED);
+    return (($opt || '') =~ /unsigned/) ? int(rand() * $MAX_INT_UNSIGNED) : int(rand() * $MAX_INT_SIGNED);
 }
 
 
 sub val_datetime {
-    return DateTime->from_epoch( epoch => rand() * $MAX_INT_UNSIGNED )->datetime();
+    return DateTime->from_epoch( epoch => time + rand() * 2 * $ONE_YEAR_SEC - $ONE_YEAR_SEC )->datetime();
 }
 
 
+sub get_current_ref_keys {
+    my ($dbh, $table, $col) = @_;
+
+    #  現存するレコードを確認
+    my $ref_sql = "SELECT DISTINCT $col FROM $table LIMIT 100";
+    my $ref_res = $dbh->selectall_arrayref($ref_sql);
+
+    return $ref_res;
+}
