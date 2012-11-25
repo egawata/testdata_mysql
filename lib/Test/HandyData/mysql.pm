cdc3252a2e1ccfecda3d52c1a666fd8c0cd40ddc
Egawa Takashi <egawa.takashi@gmail.com>
2012-11-25 21:44:04 +0900

    make module

---
+++ lib/Test/HandyData/mysql.pm
@@ -0,0 +1,521 @@
+package Test::HandyData::mysql;
+
+use strict;
+use warnings;
+
+our $VERSION = '0.0.1';
+
+
+use DBI;
+use Data::Dumper;
+use DateTime;
+use Carp;
+use Class::Accessor::Lite (
+    new     => 1,
+    rw      => [
+        'dbh',          #  Database handle
+        'fk',           #  1: Creates record on other table referenced by main table
+        'nonull',       #  1: Assigns values to columns even if those default is NULL.      
+        'cond',
+        'cond_ref',
+    ],
+);
+
+
+###############
+#
+#  Constants
+#
+###############
+
+my $ONE_YEAR_SEC = 86400 * 365;
+
+my @VARCHAR_LIST = ( 0..9, 'a'..'z', 'A'..'Z', '_' );
+my $COUNT_VARCHAR_LIST = scalar @VARCHAR_LIST;
+
+my $MAX_TINYINT_SIGNED       = 127;
+my $MAX_TINYINT_UNSIGNED     = 255;
+my $MAX_SMALLINT_SIGNED      = 32767;
+my $MAX_SMALLINT_UNSIGNED    = 65535;
+my $MAX_INT_SIGNED           = 2147483647;
+my $MAX_INT_UNSIGNED         = 4294967295;
+
+my %VALUE_DEF_FUNC = (
+    char        => \&val_varchar,
+    varchar     => \&val_varchar,
+    tinyint     => \&val_tinyint,
+    smallint    => \&val_smallint,
+    int         => \&val_int,
+    bigint      => \&val_int,
+    float       => \&val_float,
+    double      => \&val_float,
+    datetime    => \&val_datetime,
+    timestamp   => \&val_datetime,
+    date        => \&val_datetime,
+);
+
+
+=head1 NAME
+ 
+Test::HandyData::mysql - Generates handy test data for mysql 
+ 
+ 
+=head1 VERSION
+ 
+This documentation refers to Test::HandyData::mysql version 0.0.1
+ 
+ 
+=head1 SYNOPSIS
+
+    use DBI;
+    use Test::HandyData::mysql;
+   
+    my $dbh = DBI->connect('dbi:mysql:test', 'user', 'pass');
+
+    my $handy = Test::HandyData::mysql->new( fk => 1 );
+    $handy->dbh($dbh);
+    
+    
+    #  -- table definitions --
+    #
+    #  create table table1 (
+    #      id           integer primary key auto_increment,
+    #      group_id     interger not null,
+    #      name         varchar(20) not null,
+    #      price        integer not null,
+    #      constraint foreign key group_id references group(id)
+    #  );
+    #  
+    #  create table group (
+    #      id           integer primary key,
+    #      name         varchar(20) not null
+    #  ); 
+    #
+
+
+    #
+    #  Insert one row to 'table1'.
+    #  'group_id', 'name' and 'price' will be random values.
+    #  'group_id' refers to group(id), so the value will be selected one of ids in table 'group'.
+    #  If table 'group' has no record, new record will be generated on 'group'. 
+    #
+    $handy->insert('table1');
+    
+        
+    #
+    #  Insert one row to 'table1' with name = 'Banana'
+    #  group_id and price will be random values. 
+    #
+    $handy->insert('table1', { name => 'Banana' });
+    
+    
+    #
+    #  Insert one row to 'table1' with group_id one of 10, 20 or 30 (selected randomly)  
+    #  If table 'group' has no record with id = 10, 20 nor 30, 
+    #  3 records having those ids will be generated on 'group'.
+    #
+    $handy->insert('table1', { group_id => [ 10, 20, 30 ] });
+
+  
+=head1 DESCRIPTION
+
+テスト用のデータを生成し、mysql のテーブルに INSERT します。
+
+開発初期の動作確認時に、データベーステーブル上の列のうち、テストに関係のある列のみを指定し、他の列はどのような値でも構わないということがよくあります。しかし NOT NULL や FOREIGN KEY などの制約がある場合、関心のないデータ、さらには関心のない他のテーブル上のデータについてすべて指定しなければなりません。これは煩わしいことです。
+
+このモジュールは、関心がある列以外のデータをランダムに生成し、テーブルへレコードを INSERT することを可能にします。また必要であれば、外部参照先のレコードも同時に生成します。
+
+
+ 
+=head1 METHODS 
+ 
+
+=cut 
+
+=head2 new(%params)
+
+
+=head2 dbh($dbh)
+
+passes database handle
+
+
+=head2 fk($bool)
+
+creates records on other tables referred by foreign key columns in main table, if necessary.
+
+
+=head2 nonull($bool)
+
+assigns values to columns even if those defaults are NULL.
+
+
+=head2 insert($table, $cond)
+
+Inserts a record.
+
+$cond is a hashref which keys are columns' names in $table.
+
+
+=item colname => $scalar
+
+specifies a value of 'colname'
+
+    $handy->insert('table1', { id => 5 });      #  id is 5
+
+
+=item colname => [ $val1, $val2, ... ]
+
+value of 'colname' is decided as one of $val1, $val2, ... randomly.
+
+    $handy->insert('table1', { id => [ 10, 20, 30 ] })      #  id is one of 10, 20, 30
+
+
+=item colname => { random => [ $val1, $val2, ... ] }
+
+verbose expression of above
+
+
+=item colname => { fk => 1 }
+
+creates records on other tables referred by foreign key columns in main table, if necessary. (Overrides object's "fk" attribute)
+
+
+=item colname => { nonull => $bool }
+
+assigns/doesn't assign value to the column even if its default is NULL. (Overrides object's "nonull" attribute)
+
+    $handy->insert('table1', { group_id => { random => [ 10, 20, 30 ], fk => 1, nonull => 1 } });
+
+
+=cut
+
+sub insert {
+    my ($self, $table, $table_cond) = @_;
+
+    #  再帰処理を行う前に、前回の条件をクリアする必要がある(外部参照以外)
+    $self->cond({});
+    $self->cond_ref() or $self->cond_ref({}); 
+
+    #  新しい条件を読み込む
+    $self->parse_table_cond($table, $table_cond);
+
+    return $self->process_table($table, $table_cond);
+}
+
+
+
+#  1つのテーブルに1レコードを追加する。
+#  戻り値は、INSERT されたレコードの ID。
+sub process_table {
+    my ($self, $table, $table_cond) = @_;
+    my $dbh = $self->dbh();
+
+
+    my $def = $self->get_table_definition($table);
+    my $constraint = $self->get_constraint($table);
+
+
+    #  値を指定する必要のある列のみ抽出する
+    my @colnames = $self->get_cols_requires_value($table, $def);
+
+    my $cols = join ',', @colnames;
+    my $ph   = join ',', ('?') x scalar(@colnames);
+    my $sql = "INSERT INTO $table ($cols) VALUES ($ph)";
+
+    my $sth = $dbh->prepare($sql);
+
+    {
+        my @values = ();
+        for my $key (@colnames) {
+
+            my $type = $def->{$key}{DATA_TYPE};
+            my $size = $def->{$key}{CHARACTER_MAXIMUM_LENGTH};
+            my $opt  = $def->{$key}{COLUMN_TYPE};
+
+            #  外部キー制約の有無を確認
+            my $const_key = $constraint->{$key};
+            if ( defined $const_key 
+                and $const_key->{REFERENCED_TABLE_SCHEMA} 
+                and my $ref_table = $const_key->{REFERENCED_TABLE_NAME} 
+                and my $ref_col   = $const_key->{REFERENCED_COLUMN_NAME} ) 
+            {
+
+                if ( !defined( $self->cond_ref()->{$table}{$key} ) ) {
+                    my $ref_res = $self->get_current_ref_keys($ref_table, $ref_col); 
+                    if ( @$ref_res ) {
+                        $self->cond_ref()->{$table}{$key}{random} = $ref_res;
+                    }
+                    else {
+                        my $ref_keys = $self->process_table($ref_table);
+                        $ref_res = $self->get_current_ref_keys($ref_table, $ref_col);
+                        if ( @$ref_res ) {
+                            $self->cond_ref()->{$table}{$key}{random} = $ref_res;
+                        }
+                        else {
+                            die "Something is wrong\n";
+                        }
+                    } 
+                }
+
+            }
+
+            my $value;
+
+
+            #  列に値決定のルールが設定されていればそれを使う
+            for ( $self->cond(), $self->cond_ref() ) {
+                $value = determine_value( $_->{$table}{$key} );
+                defined($value) and last;
+            }
+
+
+            #  ルールが設定されていなければ、ランダムに値を決定する
+            if ( !defined($value) ) {
+                my $func = $VALUE_DEF_FUNC{$type}
+                    or die "Type $type for $key not supported";
+                
+                $value = $func->($size, $opt);
+            }
+
+            push @values, $value;
+        }
+
+        $sth->execute(@values);
+    }
+
+    $sth->finish;
+
+    return $dbh->{'mysql_insertid'};
+}
+
+
+
+#  ルールにしたがって列値を決定する
+sub determine_value {
+    my ($cond_key) = @_;
+
+    my $value;
+
+    if ( $cond_key->{random} ) {
+        my $ind = rand() * @{ $cond_key->{random} };
+        $value = $cond_key->{random}[$ind]; 
+    }
+    elsif ( $cond_key->{fixval} ) {
+        $value = $cond_key->{fixval};
+    }
+
+    return $value;
+}
+
+
+#  INSERT 実行時に値を指定する必要のある列のみ抽出する
+sub get_cols_requires_value {
+    my ($self, $table, $def) = @_;
+
+    return grep { 
+        defined( $self->cond()->{$table}{$_} )
+        or (
+            $def->{$_}{EXTRA} !~ /auto_increment/           #  auto_increment 列
+            and not defined($def->{$_}{COLUMN_DEFAULT})     #  default 値が指定済み
+        )
+    } keys %$def;
+}
+
+
+sub dbname {
+    my ($self) = @_;
+
+    unless ( $self->{dbname} ) {
+        my $res = $self->dbh()->selectall_arrayref('SELECT DATABASE()');
+        $self->{dbname} = $res->[0]->[0]
+            or confess "Failed to get dbname";
+    }
+    
+    return $self->{dbname}; 
+}
+
+
+sub get_table_definition {
+    my ($self, $table) = @_;
+
+    my $sql = "SELECT * FROM information_schema.columns WHERE table_schema = ? AND table_name = ?";
+    my $res = $self->dbh()->selectall_hashref($sql, 'COLUMN_NAME', undef, $self->dbname(), $table);
+
+    return $res;
+}
+
+
+sub get_constraint {
+    my ($self, $table) = @_;
+
+    my $sql = "SELECT * FROM information_schema.key_column_usage where table_schema = ? AND table_name = ?";
+    my $res = $self->dbh()->selectall_hashref($sql, 'COLUMN_NAME', undef, $self->dbname(), $table);
+    
+    return $res;  
+}
+
+sub val_varchar {
+    my ($size) = @_;
+
+    my $string = '';
+    for (1 .. $size) {
+        $string .= $VARCHAR_LIST[ int( rand() * $COUNT_VARCHAR_LIST ) ];
+    }
+
+    return $string;
+}
+
+
+sub val_tinyint {
+    my ($size, $opt) = @_;
+
+    return (($opt || '') =~ /unsigned/) ? int(rand() * $MAX_TINYINT_UNSIGNED) : int(rand() * $MAX_TINYINT_SIGNED);
+}
+
+sub val_smallint {
+    my ($size, $opt) = @_;
+
+    return (($opt || '') =~ /unsigned/) ? int(rand() * $MAX_SMALLINT_UNSIGNED) : int(rand() * $MAX_SMALLINT_SIGNED);
+}
+
+sub val_int {
+    my ($size, $opt) = @_;
+
+    return (($opt || '') =~ /unsigned/) ? int(rand() * $MAX_INT_UNSIGNED) : int(rand() * $MAX_INT_SIGNED);
+}
+
+
+sub val_float {
+    my ($size, $opt) = @_;
+
+    return (($opt || '') =~ /unsigned/) ? rand() * $MAX_INT_UNSIGNED : rand() * $MAX_INT_SIGNED;
+}
+
+
+sub val_datetime {
+    return DateTime->from_epoch( epoch => time + rand() * 2 * $ONE_YEAR_SEC - $ONE_YEAR_SEC )->datetime();
+}
+
+
+sub get_current_ref_keys {
+    my ($self, $table, $col) = @_;
+
+    #  現存するレコードを確認
+    my $ref_sql = "SELECT DISTINCT $col FROM $table LIMIT 100";
+    my $ref_res = $self->dbh()->selectall_arrayref($ref_sql);
+
+    return [ map { $_->[0] } @$ref_res ];
+}
+
+
+sub parse_table_cond {
+    my ($self, $table, $table_cond) = @_;
+
+    return unless $table_cond and ref $table_cond eq 'HASH';
+
+    for my $col (keys %$table_cond) {
+        
+        my $_table = $table;
+        my $_col   = $col;
+
+        if ( $col =~ /^\w+\.\w+$/ ) {
+            ($_table, $_col) = split '\.', $col;
+        }
+
+        my $val = $table_cond->{$col};
+
+        if ( ref $val eq 'ARRAY' ) {
+            $self->cond()->{$_table}{$_col}{random} = $val;
+        }
+        elsif ( ref $val eq 'HASH' ) {
+            for (keys %$val) {
+                $self->cond()->{$_table}{$_col}{$_} = $val->{$_};
+            }
+        }
+        elsif ( ref $val eq '' ) {
+            $self->cond()->{$_table}{$_col}{fixval} = $val;
+        }
+    }
+
+}   
+
+
+
+
+
+1;
+
+
+
+__END__
+
+=head1 DIAGNOSTICS
+ 
+A list of every error and warning message that the module can generate
+(even the ones that will "never happen"), with a full explanation of each 
+problem, one or more likely causes, and any suggested remedies.
+(See also  QUOTE \" " INCLUDETEXT "13_ErrorHandling" "XREF83683_Documenting_Errors_"\! Documenting Errors QUOTE \" " QUOTE " in Chapter "  in Chapter  INCLUDETEXT "13_ErrorHandling" "XREF40477__"\! 13.)
+ 
+ 
+=head1 CONFIGURATION AND ENVIRONMENT
+ 
+A full explanation of any configuration system(s) used by the module,
+including the names and locations of any configuration files, and the
+meaning of any environment variables or properties that can be set. These
+descriptions must also include details of any configuration language used.
+(also see  QUOTE \" " INCLUDETEXT "19_Miscellanea" "XREF40334_Configuration_Files_"\! Configuration Files QUOTE \" " QUOTE " in Chapter "  in Chapter  INCLUDETEXT "19_Miscellanea" "XREF55683__"\! 19.)
+ 
+ 
+=head1 DEPENDENCIES
+ 
+A list of all the other modules that this module relies upon, including any
+restrictions on versions, and an indication whether these required modules are
+part of the standard Perl distribution, part of the module's distribution,
+or must be installed separately.
+ 
+ 
+=head1 INCOMPATIBILITIES
+ 
+A list of any modules that this module cannot be used in conjunction with.
+This may be due to name conflicts in the interface, or competition for 
+system or program resources, or due to internal limitations of Perl 
+(for example, many modules that use source code filters are mutually 
+incompatible).
+ 
+ 
+=head1 BUGS AND LIMITATIONS
+ 
+A list of known problems with the module, together with some indication
+whether they are likely to be fixed in an upcoming release.
+ 
+Also a list of restrictions on the features the module does provide: 
+data types that cannot be handled, performance issues and the circumstances
+in which they may arise, practical limitations on the size of data sets, 
+special cases that are not (yet) handled, etc.
+ 
+The initial template usually just has:
+ 
+There are no known bugs in this module. 
+Please report problems to <Maintainer name(s)>  (<contact address>)
+Patches are welcome.
+ 
+=head1 AUTHOR
+ 
+<Author name(s)>  (<contact address>)
+ 
+ 
+=head1 LICENCE AND COPYRIGHT
+ 
+Copyright (c) <year> <copyright holder> (<contact address>). All rights reserved.
+ 
+Followed by whatever licence you wish to release it under. 
+For Perl code that is often just:
+ 
+This module is free software; you can redistribute it and/or
+modify it under the same terms as Perl itself. See L<perlartistic>.
+ 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. 
+
---
+++ misc/modtest.pl
@@ -0,0 +1,48 @@
+use strict;
+use warnings;
+
+use lib qw(lib);
+
+use DBI;
+use Test::HandyData::mysql;
+
+
+main();
+exit(0);
+
+sub main {
+
+    my $dbh = DBI->connect('dbi:mysql:test', 'root', undef, { RaiseError => 1 })
+        or die $DBI::errstr;
+
+    $dbh->do('DROP TABLE IF EXISTS table1');
+    $dbh->do('DROP TABLE IF EXISTS category');
+    $dbh->do(<<SQL);      
+        create table category (
+            id           integer primary key,
+            name         varchar(20) not null
+        )
+SQL
+    $dbh->do(<<SQL);
+        CREATE TABLE table1 (
+            id           integer primary key auto_increment,
+            category_id  integer not null,
+            name         varchar(20) not null,
+            price        integer not null,
+            constraint foreign key (category_id) references category(id)
+        )
+SQL
+
+    my $handy = Test::HandyData::mysql->new();
+    $handy->fk(1);
+    $handy->dbh($dbh);
+    
+    #$handy->insert('table1');
+    $handy->insert('table1', { name => ['Apple', 'Banana', 'Coconut' ], 'category.id' => [ 10, 20, 30 ] }) for 1 .. 10;
+     
+    $dbh->disconnect();
+       
+} 
+    
+
+
